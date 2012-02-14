faec0ece6e2b348b5faa034227655e3243247f9a
Jan Marek <mail@janmarek.net>
2012-02-14 20:36:32 +0100

    less filter (with lessphp)

---
+++ Filter/LessFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Webloader\Filter;
+
+/**
+ * Less CSS filter
+ *
+ * @author Jan Marek
+ * @license MIT
+ */
+class LessFilter
+{
+
+	private $lc;
+
+	/**
+	 * @return \lessc
+	 */
+	private function getLessC()
+	{
+		// lazy loading
+		if (empty($this->lc)) {
+			$this->lc = new \lessc();
+		}
+
+		return $this->lc;
+	}
+
+	/**
+	 * Invoke filter
+	 * @param string $code
+	 * @param \WebLoader\Compiler $loader
+	 * @param string $file
+	 * @return string
+	 */
+	public function __invoke($code, \WebLoader\Compiler $loader, $file)
+	{
+		if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
+			return $this->getLessC()->parse($code);
+		}
+
+		return $code;
+	}
+
+}
