b89084ad2c7d8209e235ad11012fbe2ba3cba5f3
Jan Marek <mail@janmarek.net>
2012-03-19 11:38:36 +0100

    WebLoader is fully compatible with composer and PSR-0 loaders now

---
+++ tests/bootstrap.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/../WebLoader/WebLoaderException.php';
+require __DIR__ . '/../WebLoader/FileNotFoundException.php';
+require __DIR__ . '/../WebLoader/InvalidArgumentException.php';
+require __DIR__ . '/../WebLoader/IFileCollection.php';
+require __DIR__ . '/../WebLoader/FileCollection.php';
+require __DIR__ . '/../WebLoader/Compiler.php';
+require __DIR__ . '/../WebLoader/IOutputNamingConvention.php';
+require __DIR__ . '/../WebLoader/DefaultOutputNamingConvention.php';
+require __DIR__ . '/../WebLoader/Filter/CssUrlsFilter.php';
+require __DIR__ . '/../WebLoader/Filter/VariablesFilter.php';
