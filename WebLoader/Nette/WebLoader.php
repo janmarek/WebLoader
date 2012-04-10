172a0fdca4dc0c536e9321f17efd2d50f006d8f3
Pavel Zelezny <pavel.zelezny@socialbakers.com>
2012-04-10 08:53:42 +0200

    Remote files from template

--- WebLoader/Nette/WebLoader.php
+++ WebLoader/Nette/WebLoader.php
@@ -70,28 +70,40 @@ abstract class WebLoader extends \Nette\Application\UI\Control
 	 * Generate compiled file(s) and render link(s)
 	 */
 	public function render()
-	{
-		$hasArgs = func_num_args() > 0;
+		{
+			$hasArgs = func_num_args() > 0;
 
-		if ($hasArgs) {
-			$backup = $this->compiler->getFileCollection();
-			$newFiles = new FileCollection($backup->getRoot());
-			$newFiles->addFiles(func_get_args());
-			$this->compiler->setFileCollection($newFiles);
-		}
+			if ($hasArgs) {
+				$localFiles = array();
+				$remoteFiles = array();
+				foreach (func_get_args() as $file) {
+					if(preg_match('/^(http|https)/', $file)) {
+						$remoteFiles[] = $file;
+					}
+					else {
+						$localFiles[] = $file;
+					}
+				}
 
-		// remote files
-		foreach ($this->compiler->getFileCollection()->getRemoteFiles() as $file) {
-			echo $this->getElement($file);
-		}
+				$backup = $this->compiler->getFileCollection();
+				$newFiles = new FileCollection($backup->getRoot());
+				$newFiles->addFiles($localFiles);
+				$newFiles->addRemoteFiles($remoteFiles);
+				$this->compiler->setFileCollection($newFiles);
+			}
 
-		foreach ($this->compiler->generate() as $file) {
-			echo $this->getElement($this->tempPath . '/' . $file->file . (($file->lastModified !== NULL) ? '?' . $file->lastModified : ''));
-		}
+			// remote files
+			foreach ($this->compiler->getFileCollection()->getRemoteFiles() as $file) {
+				echo $this->getElement($file);
+			}
 
-		if ($hasArgs) {
-			$this->compiler->setFileCollection($backup);
+			foreach ($this->compiler->generate() as $file) {
+				echo $this->getElement($this->tempPath . '/' . $file->file . (($file->lastModified !== NULL) ? '?' . $file->lastModified : ''));
+			}
+
+			if ($hasArgs) {
+				$this->compiler->setFileCollection($backup);
+			}
 		}
-	}
 
 }
