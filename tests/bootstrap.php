29d088c5832038a9f6d5dc8a9d1f0732eef1fffb
Jan Marek <mail@janmarek.net>
2012-09-18 10:02:27 +0200

    simplified test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,3 @@
 <?php
 
-require __DIR__ . '/../WebLoader/WebLoaderException.php';
-require __DIR__ . '/../WebLoader/FileNotFoundException.php';
-require __DIR__ . '/../WebLoader/InvalidArgumentException.php';
-require __DIR__ . '/../WebLoader/IFileCollection.php';
-require __DIR__ . '/../WebLoader/FileCollection.php';
-require __DIR__ . '/../WebLoader/Compiler.php';
-require __DIR__ . '/../WebLoader/IOutputNamingConvention.php';
-require __DIR__ . '/../WebLoader/DefaultOutputNamingConvention.php';
-require __DIR__ . '/../WebLoader/Filter/CssUrlsFilter.php';
-require __DIR__ . '/../WebLoader/Filter/VariablesFilter.php';
 require __DIR__ . '/../vendor/autoload.php';
