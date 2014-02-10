665988c531aebfc27b9782fd2584287d6b93786f
Radovan Kepák <radovan@kepak.eu>
2014-02-10 11:36:44 +0100

    Přidáno klonování LessC
    Pokud pouzijeme import jednoho souboru s funkcema do dvou ruznych Less
    souboru, provede se import pouze do prvniho, v druhem jiz instance lessc
    pozna, ze soubor byl importovan a vynecha jeho import, je proto treba
    pro kazdy soubor vytvorit novou instanci lessc, tak aby byl vzdy soubor
    importovan

--- WebLoader/Filter/LessFilter.php
+++ WebLoader/Filter/LessFilter.php
@@ -8,44 +8,40 @@ namespace WebLoader\Filter;
  * @author Jan Marek
  * @license MIT
  */
-class LessFilter
-{
+class LessFilter {
 
-	private $lc;
+    private $lc;
 
-	public function __construct(\lessc $lc = NULL)
-	{
-		$this->lc = $lc;
-	}
+    public function __construct(\lessc $lc = null) {
+        $this->lc = $lc;
+    }
 
-	/**
-	 * @return \lessc
-	 */
-	private function getLessC()
-	{
-		// lazy loading
-		if (empty($this->lc)) {
-			$this->lc = new \lessc();
-		}
+    /**
+     * @return \lessc
+     */
+    private function getLessC() {
+        // lazy loading
+        if (empty($this->lc)) {
+            $this->lc = new \lessc();
+        }
 
-		return $this->lc;
-	}
+        return clone $this->lc;
+    }
 
-	/**
-	 * Invoke filter
-	 * @param string $code
-	 * @param \WebLoader\Compiler $loader
-	 * @param string $file
-	 * @return string
-	 */
-	public function __invoke($code, \WebLoader\Compiler $loader, $file)
-	{
-		if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
-			$this->getLessC()->importDir = pathinfo($file, PATHINFO_DIRNAME) . '/';
-			return $this->getLessC()->parse($code);
-		}
+    /**
+     * Invoke filter
+     * @param string $code
+     * @param \WebLoader\Compiler $loader
+     * @param string $file
+     * @return string
+     */
+    public function __invoke($code, \WebLoader\Compiler $loader, $file) {
+        if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
+            $lessc = $this->getLessC();
+            $lessc->importDir = pathinfo($file, PATHINFO_DIRNAME) . '/';
+            return $lessc->parse($code);
+        }
 
-		return $code;
-	}
-
+        return $code;
+    }
 }
