22c816a7595d6730901a0fe035d13176f31e7d34
Radovan Kepák <radovan@kepak.eu>
2014-02-11 11:58:03 +0100

    * Fixed coding standard

--- WebLoader/Filter/LessFilter.php
+++ WebLoader/Filter/LessFilter.php
@@ -11,41 +11,41 @@ namespace WebLoader\Filter;
 class LessFilter
 {
 
-    private $lc;
+	private $lc;
 
-    public function __construct(\lessc $lc = null)
-    {
-        $this->lc = $lc;
-    }
+	public function __construct(\lessc $lc = null)
+	{
+		$this->lc = $lc;
+	}
 
-    /**
-     * @return \lessc
-     */
-    private function getLessC()
-    {
-        // lazy loading
-        if (empty($this->lc)) {
-            $this->lc = new \lessc();
-        }
+	/**
+	 * @return \lessc
+	 */
+	private function getLessC()
+	{
+		// lazy loading
+		if (empty($this->lc)) {
+			$this->lc = new \lessc();
+		}
 
-        return clone $this->lc;
-    }
+		return clone $this->lc;
+	}
 
-    /**
-     * Invoke filter
-     * @param string $code
-     * @param \WebLoader\Compiler $loader
-     * @param string $file
-     * @return string
-     */
-    public function __invoke($code, \WebLoader\Compiler $loader, $file)
-    {
-        if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
-            $lessc = $this->getLessC();
-            $lessc->importDir = pathinfo($file, PATHINFO_DIRNAME) . '/';
-            return $lessc->parse($code);
-        }
+	/**
+	 * Invoke filter
+	 * @param string $code
+	 * @param \WebLoader\Compiler $loader
+	 * @param string $file
+	 * @return string
+	 */
+	public function __invoke($code, \WebLoader\Compiler $loader, $file)
+	{
+		if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
+			$lessc = $this->getLessC();
+			$lessc->importDir = pathinfo($file, PATHINFO_DIRNAME) . '/';
+			return $lessc->parse($code);
+		}
 
-        return $code;
-    }
+		return $code;
+	}
 }
