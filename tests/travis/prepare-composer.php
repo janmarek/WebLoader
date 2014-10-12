3b2489ac67a69bf8da71e20a5c424b15c9526afb
Filip Procházka <filip@prochazka.su>
2014-10-13 00:29:23 +0200

    Travis: fixed prepare-composer.php

--- tests/travis/prepare-composer.php
+++ tests/travis/prepare-composer.php
@@ -7,10 +7,14 @@ if (getenv('NETTE') !== 'default') {
 	$composerFile = $travisDir . '/composer-' . getenv('NETTE') . '.json';
 
 	unlink($rootDir . '/composer.json');
-	copy($composerFile, $rootDir . '/composer.json');
+	if (!copy($composerFile, $rootDir . '/composer.json')) {
+		exit(1);
+	}
 
 	echo "Using tests/", basename($composerFile);
 
 } else {
 	echo "Using default composer.json";
 }
+
+exit(0);
