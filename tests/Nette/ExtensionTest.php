253c81f56eccd30069f912f1f4801e54c58ef164
Filip Procházka <filip@prochazka.su>
2014-10-13 01:50:23 +0200

    dead code

--- tests/Nette/ExtensionTest.php
+++ tests/Nette/ExtensionTest.php
@@ -4,13 +4,6 @@ namespace WebLoader\Test\Nette;
 
 use Nette\Utils\Finder;
 
-if (!class_exists('Nette\DI\CompilerExtension')) {
-	class_alias('Nette\Config\CompilerExtension', 'Nette\DI\CompilerExtension');
-	class_alias('Nette\Config\Compiler', 'Nette\DI\Compiler');
-	class_alias('Nette\Config\Helpers', 'Nette\DI\Config\Helpers');
-	class_alias('Nette\Config\Configurator', 'Nette\Configurator');
-}
-
 class ExtensionTest extends \PHPUnit_Framework_TestCase
 {
 
