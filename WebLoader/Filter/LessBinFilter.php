fc0384c4965cdd2245a078e5658461bbc90f05f4
Jan Tvrdik <jan.tvrdik@gmail.com>
2015-03-08 07:25:44 +0100

    added LessFilter2

---
+++ WebLoader/Filter/LessBinFilter.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace WebLoader\Filter;
+
+/**
+ * Less CSS filter
+ *
+ * @author Jan Tvrdík
+ * @license MIT
+ */
+class LessBinFilter
+{
+
+	/** @var string */
+	private $bin;
+
+	/** @var array */
+	private $env;
+
+	/**
+	 * @param string $bin
+	 * @param array $env
+	 */
+	public function __construct($bin = 'lessc', array $env = array())
+	{
+		$this->bin = $bin;
+		$this->env = $env + $_ENV;
+		unset($this->env['argv'], $this->env['argc']);
+	}
+
+	/**
+	 * Invoke filter
+	 * @param string $code
+	 * @param \WebLoader\Compiler $loader
+	 * @param string $file
+	 * @return string
+	 */
+	public function __invoke($code, \WebLoader\Compiler $loader, $file)
+	{
+		if (pathinfo($file, PATHINFO_EXTENSION) === 'less') {
+			$code = Process::run("{$this->bin} -", $code, dirname($file), $this->env);;
+		}
+
+		return $code;
+	}
+
+}
