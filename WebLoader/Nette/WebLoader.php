828741c09e9fe221777c4203343a2cd3cdd05e2b
Adam Zemek <eda.bmhd@centrum.cz>
2020-10-19 17:19:03 +0200

    WebLoader component: compatibility with Nette 3

--- WebLoader/Nette/WebLoader.php
+++ WebLoader/Nette/WebLoader.php
@@ -22,7 +22,6 @@ abstract class WebLoader extends \Nette\Application\UI\Control
 
 	public function __construct(Compiler $compiler, $tempPath)
 	{
-		parent::__construct();
 		$this->compiler = $compiler;
 		$this->tempPath = $tempPath;
 	}
